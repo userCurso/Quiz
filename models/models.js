6aea9b8ff8f74c086137589d5064f9bded98dc5d
JavierBmrg <JavierBmrg@openaliasbox.org>
2015-07-12 12:58:01 -0430

    Despliegue en Heroku.

--- models/models.js
+++ models/models.js
@@ -1,12 +1,25 @@
 var path = require('path');
 
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name = (url[6]||null);
+var user = (url[2]||null);
+var pwd = (url[3]||null);
+var protocol = (url[1]||null);
+var dialect = (url[1]||null);
+var port = (url[5]||null);
+var host = (url[4]||null);
+var storage = process.env.DATABASE_STORAGE;
+
+
 var Sequelize = require('sequelize');
 
-var sequelize = new Sequelize('quiz', null, null,
-                      {dialect: "sqlite", storage: "quiz.sqlite"}
+var sequelize = new Sequelize(DB_name, user, pwd,
+                      {dialect: protocol, protocol: protocol, port: port, host: host, storage: storage, omitNull: true}
                     );
 
-var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
+var quiz_path = path.join(__dirname, 'quiz');
+
+var Quiz = sequelize.import(quiz_path);
 
 exports.Quiz = Quiz;
 
