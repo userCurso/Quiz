670eeafdc34b7bc924fc03e4e1288e1ead71b24a
JavierBmrg <JavierBmrg@openaliasbox.org>
2015-07-12 17:36:09 -0430

    responsive zoom

--- models/models.js
+++ models/models.js
@@ -27,7 +27,8 @@ sequelize.sync().then(function(){
   Quiz.count().then(function(count){
     if(count === 0){
       Quiz.create({ pregunta: 'Capital de Italia',respuesta: 'Roma'});
-      Quiz.create({ pregunta: 'Capital de Portugal',respuesta: 'Lisboa'})
+      Quiz.create({ pregunta: 'Capital de Portugal',respuesta: 'Lisboa'});
+      Quiz.create({ pregunta: 'Capital de Siria',respuesta: 'Damasco'})
       .then(function(){console.log('Base de datos inicializada')});
     };
   });
